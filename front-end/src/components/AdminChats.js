cc2eca91f843341f368c16819cde5238141d9677
nato-re <alvares.renato@hotmail.com>
2021-01-14 10:38:08 -0300

    add rubric

---
+++ front-end/src/components/AdminChats.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import axios from 'axios';
+import { ContextAplication } from '../context/ContextAplication';
+
+const one = 1;
+
+export default function AdminChats() {
+  const [chats, setChats] = useState([]);
+  const { setUserChat } = useContext(ContextAplication);
+  const history = useHistory();
+
+  useEffect(() => {
+    let isSubscribed = true;
+    const getChats = async () => {
+      const { token } = JSON.parse(localStorage.getItem('user'));
+
+      const response = await axios.get('http://localhost:3001/admin/chats', { headers: { authorization: token } });
+      const chatsList = await response.data;
+
+      if (isSubscribed) setChats(chatsList);
+    };
+    getChats();
+    return () => isSubscribed = false;
+  }, []);
+  // useEffect(() => {
+  //   return () => {
+  //     mountedRef.current = false
+  //   }
+  // }, [])
+  return !chats ? <h2>Não há conversas</h2> : (
+    <div>
+      <ul>
+        {console.log('chats: ', chats)}
+        { !!chats.length && chats.map((chat) => (
+          <li key={ chat.email }>
+            <button
+              type="button"
+              onClick={ () => {
+                localStorage.setItem('chat', chat.email)
+                setUserChat(chat.email);
+                history.push('/chat');
+              } }
+            >
+
+              <div data-testid="containerChat">
+                <p data-testid="profile-name">{chat.email}</p>
+                <p data-testid="last-message">{chat.messages[chat.messages.length - one].text}</p>
+              </div>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>);
+}
---
+++ front-end/src/components/Chat.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Redirect, Link } from 'react-router-dom';
+import axios from 'axios';
+import './Chat.css';
+import { ClientMenu, AdminMenu } from './Menu/index';
+
+import { ContextAplication } from '../context/ContextAplication';
+
+const io = require('socket.io-client');
+
+const socket = io('http://localhost:3001', { transports: ['websocket'] });
+
+
+function ClientChat() {
+  const { userChat } = useContext(ContextAplication);
+  const [chat, setChat] = useState([]);
+  const localUser = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem('user')) || null;
+
+  const emitHistory = () => 
+    {
+      console.log('userChat:', userChat)
+      localUser.role === 'administrator'
+    ? socket.emit('history', { email: localStorage.getItem('chat') })
+    : socket.emit('history', user)
+  };
+
+  useEffect(emitHistory, []);
+
+  socket.on('message', ({ newMessage }) => {
+    const { from, time, text } = newMessage;
+    setChat([...chat, { from, time, text }]);
+  });
+
+  socket.on('history', (previousMessages) => {
+    setChat(previousMessages);
+  });
+
+  const displayMessage = () => {
+    const message = document.getElementById('message-input').value;
+    if (user.role === 'administrator') {
+      socket.emit('message', { message, user, to: userChat });
+    } else {
+      socket.emit('message', { message, user, to: 'Loja' });
+      document.getElementById('message-input').value = '';
+    }
+  };
+
+  // useEffect(() => {
+  //   let isSubscribed = true;
+  //   if(isSubscribed) {
+  //   const getData = async () => {
+  //     try {
+  //         const { token } = JSON.parse(localStorage.getItem('user'));
+          
+  //         await axios.get('http://localhost:3001/admin/chat', {
+  //           headers: { authorization: token },
+  //           body: { userChat },
+  //         });
+  //       } catch (e) {
+  //         console.log(e);
+  //       }
+  //     }
+  //   };
+  //   getData();
+  //   return () => isSubscribed = false;
+  // }, []);
+  useEffect(
+    () => {
+      const getData = async () => {
+        try {
+          const { token } = JSON.parse(localStorage.getItem('user'));
+  
+          await axios.get('http://localhost:3001/chat', {
+            headers: { authorization: token },
+            body: { userChat },
+        })
+        } catch (e) {
+          return e;
+        }
+        return false;
+      };
+      getData();
+    }, []
+  )
+
+  return (
+    <div>
+      {user === null && <Redirect to="/login" />}
+      {user !== null && user.role === 'administrator' ? <AdminMenu /> : <ClientMenu />}
+      <p id="hidden-text">b</p>
+      {user !== null && user.role === 'administrator' && (
+        <Link to="/admin/chats">
+          <button data-testid="back-button" type="button">
+            Voltar
+          </button>
+        </Link>)}
+      <div className="chat-div">
+        {chat.length
+          && chat.map((item) => (
+            <div key={ item.time }>
+              <p data-testid="nickname">{item.from}</p>
+              <p data-testid="message-time">{item.time}</p>
+              <p data-testid="text-message">{item.text}</p>
+            </div>
+          ))}
+      </div>
+      <input
+        id="message-input"
+        data-testid="message-input"
+      />
+      <button
+        data-testid="send-message"
+        type="button"
+        onClick={ () => displayMessage() }
+      >
+        Enviar Mensagem
+      </button>
+    </div>
+  );
+}
+
+export default ClientChat;
