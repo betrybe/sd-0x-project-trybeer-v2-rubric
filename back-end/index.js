25691ba23ded32ad6ab4750dc92cdd6f93269e42
nato-re <alvares.renato@hotmail.com>
2021-01-22 09:00:45 -0300

    testa rubrica group 6 t3

--- back-end/index.js
+++ back-end/index.js
@@ -1,81 +1,20 @@
+const http = require('http');
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const httpFactory = require('./httpFactory');
+const socketFactory = require('./socket/index');
 
-const app = express();
-const httpServer = require('http').createServer(app);
-const io = require('socket.io')(httpServer);
-const validateToken = require('./middlewares/validateToken');
+const server = express();
+server.use(bodyParser.json());
+server.use(cors());
 
-const getUserController = require('./controllers/userController');
-const getUserService = require('./services/userService');
-const { users } = require('./models');
+const httpServer = http.createServer(server);
 
-const userService = getUserService(users);
-const userController = getUserController(userService);
+const { io } = socketFactory(httpServer);
 
-const routers = require('./routers/index');
-const { errorHandler } = require('./middlewares');
+httpFactory(server, io);
 
-const { getHistory, insert } = require('./mongoModel/messageModel');
+const PORT = 3001;
 
-app.use(cors());
-app.io = io;
-
-app.use('/images', express.static(`${process.cwd()}/images`));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use('/images', express.static(`${process.cwd()}/images`));
-
-app.use('/login', routers.login(userController.loginController));
-
-app.use('/register', routers.register(userController.createUser));
-
-app.use('/profile', routers.profile(userController.updateUser));
-
-app.use('/products', routers.products);
-
-app.use('/admin', routers.admin);
-
-app.use('/orders', routers.orders);
-
-app.use('/checkout', routers.checkout);
-
-app.use('/chat', validateToken, (_req, res) => {
-  res.status(200);
-});
-
-io.on('connection', async (socket) => {
-  socket.on('message', ({ message, user, to }) => {
-    console.log(message);
-    const date = new Date();
-    const dateTime = `${date.getHours()}:${date.getMinutes()}`;
-    const newMessage = {
-      from: '',
-      to: '',
-      text: message,
-      time: dateTime,
-    };
-    if (user.role === 'administrator') {
-      newMessage.from = 'Loja';
-      newMessage.to = to;
-      insert(newMessage, to);
-    } else {
-      newMessage.from = user.email;
-      newMessage.to = to;
-      insert(newMessage, user.email);
-    }
-    io.emit('message', { newMessage });
-  });
-
-  socket.on('history', async (req) => {
-    const { email } = req;
-    const previousMessages = await getHistory(email);
-    if (previousMessages !== null) socket.emit('history', previousMessages.messages);
-  });
-});
-
-app.use(errorHandler);
-
-httpServer.listen(3001, console.log(`listen on port ${3001}`));
+httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- back-end/middlewares/index.js
+++
@@ -1,4 +0,0 @@
-const errorHandler = require('./errorHandler');
-const validateToken = require('./validateToken');
-
-module.exports = { errorHandler, validateToken };
